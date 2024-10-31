517eaee0f5c6c0eff3827214d157d11f5884bf56
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-11-01 00:08:56 +0600

    feat: add port 8080 for Jenkins

--- security-groups.tf
+++ security-groups.tf
@@ -36,6 +36,13 @@ resource "aws_security_group" "task_4_host_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
